9fbbc42f9c3b32ea07f689a261ea3c3daa37a7fa
Xingchen Hong <hello@xc-h.com>
2018-07-26 01:29:08 -0500

    Queue up beeps and make them then-able.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # beepbeep [![travis][travis-image]][travis-url] [![npm][npm-image]][npm-url] [![downloads][downloads-image]][downloads-url] [![javascript style guide][standard-image]][standard-url]
 
-[travis-image]: https://img.shields.io/travis/feross/beepbeep/master.svg
-[travis-url]: https://travis-ci.org/feross/beepbeep
-[npm-image]: https://img.shields.io/npm/v/beepbeep.svg
-[npm-url]: https://npmjs.org/package/beepbeep
-[downloads-image]: https://img.shields.io/npm/dm/beepbeep.svg
-[downloads-url]: https://npmjs.org/package/beepbeep
+[travis-image]: https://img.shields.io/travis/Zodiase/beepbeep/master.svg
+[travis-url]: https://travis-ci.org/Zodiase/beepbeep
+[npm-image]: https://img.shields.io/npm/v/@xch/beepbeep.svg
+[npm-url]: https://npmjs.org/package/@xch/beepbeep
+[downloads-image]: https://img.shields.io/npm/dm/@xch/beepbeep.svg
+[downloads-url]: https://npmjs.org/package/@xch/beepbeep
 [standard-image]: https://img.shields.io/badge/code_style-standard-brightgreen.svg
 [standard-url]: https://standardjs.com
 
@@ -14,7 +14,7 @@ Make a console beep sound. *Well-tested, web-scale, cloud-based, restful node.js
 ## Usage
 
 ```javascript
-var beep = require('beepbeep')
+var beep = require('@xch/beepbeep')
 
 beep()
 // Beep!
@@ -27,14 +27,24 @@ beep(3, 1000)
 
 beep([1000, 500, 2000])
 // 1 second delay...Beep! 0.5 second delay...Beep! 2 second delay...Beep!
+
+beep([1000, 500, 2000])
+// Beeps will queue up nicely. These will happen after the previous beeps are done.
+
+var beep2 = beep.createBeeper();
+
+beep2([1000, 500, 2000])
+// A new beeper will allow beeps to happen in parallel. These beeps will interleave the previous ones.
 ```
+
+Note that a beep takes up some time and the specified intervals are only between the start of beeps.
 
 ## Installation
 
 ```
-npm install beepbeep
+npm install @xch/beepbeep
 ```
 
 ## License
 
-MIT. Copyright (c) [Feross Aboukhadijeh](http://feross.org).
+MIT. Copyright (c) (original author) [Feross Aboukhadijeh](http://feross.org), (maintainer) Xingchen Hong.
--- index.js
+++ index.js
@@ -1,36 +1,75 @@
-'use strict'
+/**
+ * Create an async function that is resolved with the given delay.
+ * @param  {number} delay
+ * @return {Function}
+ */
+const createDelay = (delay) => () => new Promise((resolve) => setTimeout(resolve, delay))
+
+/**
+ * Returns a beep function with its own beeping queue.
+ * @return {Function}
+ */
+const createBeeper = () => {
+  const instance = {
+    queue: Promise.resolve()
+  }
+
+  return beep.bind(instance)
+}
+
+const addBeep = (instance) => {
+  instance.queue = instance.queue.then(beepNow)
+}
+
+const addDelayedBeep = (instance, delay) => {
+  const delayFunc = createDelay(delay)
+
+  instance.queue = instance.queue.then(delayFunc)
+  addBeep(instance)
+}
+
+const addPromiseResolve = (instance, resolve) => {
+  instance.queue = instance.queue.then(resolve)
+}
 
 /**
  * Make multiple console beep sounds
  * @param {number} [i=1] - Number of beeps
  * @param {number} [t=500] - Milliseconds between beeps
+ * @return {Promise}
  */
-module.exports = function (i, t) {
-  if (i instanceof Array) {
-    return delayedBeeps(i)
-  }
+function beep (i, t) {
+  const instance = this
 
-  i = isNaN(i) ? 1 : i
-  t = isNaN(t) ? 500 : t
+  return new Promise((resolve) => {
+    if (Array.isArray(i)) {
+      i.forEach(addDelayedBeep.bind(null, instance))
+      addPromiseResolve(instance, resolve)
 
-  while (i-- > 0) {
-    if (t * i === 0) beepNow()
-    else setTimeout(beepNow, t * i)
-  }
+      return
+    }
+
+    i = isNaN(i) ? 1 : i
+    t = isNaN(t) ? 500 : t
+
+    while (i-- > 0) {
+      if (t === 0) {
+        addBeep(instance)
+      } else {
+        addDelayedBeep(instance, t)
+      }
+    }
+
+    addPromiseResolve(instance, resolve)
+  })
 }
 
-function delayedBeeps (i) {
-  if (i.length === 0) {
-    return
-  }
-
-  setTimeout(function () {
-    beepNow()
-    i.splice(0, 1)
-    delayedBeeps(i)
-  }, i[0])
+async function beepNow () {
+  await process.stdout.write('\x07')
 }
 
-function beepNow () {
-  process.stdout.write('\x07')
-}
+const defaultBeeper = createBeeper()
+
+defaultBeeper.createBeeper = createBeeper
+
+module.exports = defaultBeeper
